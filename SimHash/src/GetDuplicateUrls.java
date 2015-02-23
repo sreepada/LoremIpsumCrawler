b2756f6a0c99f619a7cfdcde3767a874c4588294
madan <madanpat@usc.edu>
2015-02-22 21:23:55 -0800

    adding get duplicates test

---
+++ SimHash/src/GetDuplicateUrls.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+public class GetDuplicateUrls
+{
+    public static void main(String[] args)
+    {
+       try
+       {
+           File file_1 = new File(args[0]);
+           File file_2 = new File(args[1]);
+           Map<String, Long> objs = new HashMap<String, Long>();
+           SimhashIndex simind = new SimhashIndex(objs);
+
+           int index = 0;
+           BufferedReader br = new BufferedReader(new FileReader(file_1));
+           String line;
+           while ((line = br.readLine()) != null)
+           {
+               long simhash = SimHash.computeSimHashFromString(Shingle.shingles(line)); 
+
+               Set<String> duplicates = simind.get_near_dups(simhash);
+
+               if(duplicates.size() > 0)
+               {
+                    System.out.print("duplicate " + index);
+                    System.out.print(" duplicate_of ");
+                    for(String i: duplicates)
+                    {
+                        System.out.print(" " + i);
+                    }
+               }
+               else
+               {
+                   simind.add(String(index), simhash);
+               }
+               index += 1;
+
+           }
+
+       }
+       catch(Exception e)
+       {
+           e.printStackTrace();
+       }
+    }
+}
